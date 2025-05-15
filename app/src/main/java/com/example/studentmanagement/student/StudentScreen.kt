32021a5124eea54617d65633fa2b1ea7f4ef1752
youlong <johnyoulong@gmail.com>
2025-05-15 12:13:39 +0700

    added logout button

--- app/src/main/java/com/example/studentmanagement/student/StudentScreen.kt
+++ app/src/main/java/com/example/studentmanagement/student/StudentScreen.kt
@@ -25,6 +25,7 @@ import com.example.studentmanagement.auth.LoginScreen
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import androidx.core.content.edit
 
 class StudentScreen : AppCompatActivity() {
 
@@ -42,6 +43,21 @@ class StudentScreen : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val logoutButton = findViewById<Button>(R.id.logoutButton)
+
+        logoutButton.setOnClickListener {
+            val sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+            sharedPref.edit { clear() }
+
+            FirebaseAuth.getInstance().signOut()
+
+            val intent = Intent(this, LoginScreen::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
         }
 //
 //        FirebaseApp.initializeApp(this)
--- app/src/main/java/com/example/studentmanagement/teacher/TeacherScreen.kt
+++ app/src/main/java/com/example/studentmanagement/teacher/TeacherScreen.kt
@@ -1,11 +1,17 @@
 package com.example.studentmanagement.teacher
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.studentmanagement.R
+import androidx.core.content.edit
+import com.example.studentmanagement.auth.LoginScreen
+import com.google.firebase.auth.FirebaseAuth
 
 class TeacherScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +23,20 @@ class TeacherScreen : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val logoutButton = findViewById<Button>(R.id.logoutButton)
+
+        logoutButton.setOnClickListener {
+            val sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+            sharedPref.edit { clear() }
+
+            FirebaseAuth.getInstance().signOut()
+
+            val intent = Intent(this, LoginScreen::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
