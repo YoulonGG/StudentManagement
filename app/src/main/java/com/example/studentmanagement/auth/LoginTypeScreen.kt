ce5f16cd0006d70f6498cdd7825b6ba44ad769e3
youlong <johnyoulong@gmail.com>
2025-05-19 09:25:59 +0700

    Added Icon in login for Role selection

--- app/src/main/java/com/example/studentmanagement/auth/LoginTypeScreen.kt
+++ app/src/main/java/com/example/studentmanagement/auth/LoginTypeScreen.kt
@@ -2,7 +2,7 @@ package com.example.studentmanagement.auth
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -20,8 +20,8 @@ class LoginTypeScreen : AppCompatActivity() {
             insets
         }
 
-        val teacherBtn = findViewById<Button>(R.id.btnTeacherLogin)
-        val studentBtn = findViewById<Button>(R.id.btnStudentLogin)
+        val teacherBtn = findViewById<ImageView>(R.id.btnTeacherLogin)
+        val studentBtn = findViewById<ImageView>(R.id.btnStudentLogin)
 
         teacherBtn.setOnClickListener {
             startActivity(Intent(this, LoginScreen::class.java).apply {
