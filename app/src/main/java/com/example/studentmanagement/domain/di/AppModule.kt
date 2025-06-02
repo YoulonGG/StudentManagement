7d870db28e3f8701ebc0433df5289a1f232d8447
youlong <johnyoulong@gmail.com>
2025-06-02 13:57:42 +0700

    auth

---
+++ app/src/main/java/com/example/studentmanagement/domain/di/AppModule.kt
@@ -0,0 +1,29 @@
+package com.example.studentmanagement.domain.di
+
+/**
+ * @Author: John Youlong.
+ * @Date: 5/30/25.
+ * @Email: johnyoulong@gmail.com.
+ */
+
+import com.example.studentmanagement.presentation.approve_student.ApprovalStudentViewModel
+import com.example.studentmanagement.presentation.login.LoginViewModel
+import com.example.studentmanagement.presentation.sign_up.SignUpViewModel
+import com.example.studentmanagement.presentation.student.StudentViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+
+    single { FirebaseAuth.getInstance() }
+    single { FirebaseFirestore.getInstance() }
+
+
+    viewModel { LoginViewModel(get(), get()) }
+    viewModel { SignUpViewModel(get(), get()) }
+    viewModel { ApprovalStudentViewModel(get(), get()) }
+    viewModel { StudentViewModel(get(), get()) }
+
+}
